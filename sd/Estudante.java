7e8024ff2c3260cd9af1a45cc3ff394009c5bf67
anacatarinag9 <anacatarinajg@gmail.com>
2017-10-23 17:46:51 +0100

    yooooooo

---
+++ sd/Estudante.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd;
+
+/**
+ *
+ * @author anacj
+ */
+public class Estudante extends User{
+    
+    public Estudante(int profissao, int cc, Data validadeCC, String password, String dep, int telefone, String morada) {
+        super(profissao, cc, validadeCC, password, dep, telefone, morada);
+    }
+    
+  
+    
+    
+}
---
+++ sd/User.java
@@ -0,0 +1,82 @@
+
+package sd;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author anacj
+ */
+public class User implements Serializable{
+    protected int profissao; //1=estudante, 2=docente, 3=funcionario
+    protected int cc;
+    protected String user;//perceber se é admin, eleitor,...
+    protected String password;
+    protected String dep;
+    protected int telefone;
+    protected String morada;
+
+    protected Data validadeCC;
+    
+    
+    public User(int profissao, int cc, Data validadeCC, String password, String dep, int telefone, String morada)
+    {
+        this.profissao=profissao;
+        this.cc=cc;
+        this.validadeCC=validadeCC;
+        this.password=password;
+        this.dep=dep;
+        this.telefone=telefone;
+        this.morada=morada;
+    }
+
+    public int getTipo() {
+        return profissao;
+    }
+
+    public void setTipo(int profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDep() {
+        return dep;
+    }
+
+    public void setDep(String dep) {
+        this.dep = dep;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+    
+    
+}
